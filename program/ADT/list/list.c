254254881bdd324f571b174ab8588e258785a508
aninditaws <santosoanindita1@gmail.com>
2023-11-02 21:54:17 +0700

    update: list

--- program/ADT/list/list.c
+++ program/ADT/list/list.c
@@ -23,7 +23,7 @@ List MakeList()
 
 /* ********** TEST KOSONG/PENUH ********** */
 /* *** Test list kosong *** */
-boolean IsEmpty(List L)
+boolean IsListEmpty(List L)
 /* Mengirimkan true jika list L kosong, mengirimkan false jika tidak */
 {
     return (Length(L) == 0);
@@ -47,7 +47,7 @@ void Set(List *L, IdxType i, ElType v)
 
 /* ********** SELEKTOR ********** */
 /* *** Banyaknya elemen *** */
-int Length(List L)
+int LengthList(List L)
 /* Mengirimkan banyaknya elemen efektif list */
 /* Mengirimkan nol jika list kosong */
 {
@@ -65,14 +65,14 @@ int Length(List L)
 }
 
 /* *** Selektor INDEKS *** */
-IdxType FirstIdx(List L)
+IdxType FirstIdxList(List L)
 /* Prekondisi : list L tidak kosong */
 /* Mengirimkan indeks elemen pertama */
 {
     return 0;
 }
 
-IdxType LastIdx(List L)
+IdxType LastIdxList(List L)
 /* Prekondisi : list L tidak kosong */
 /* Mengirimkan indeks elemen terakhir */
 {
@@ -87,7 +87,7 @@ IdxType LastIdx(List L)
 }
 
 /* ********** Test Indeks yang valid ********** */
-boolean IsIdxValid (List L, IdxType i)
+boolean IsIdxListValid (List L, IdxType i)
 /* Prekondisi : i sembarang */
 /* Mengirimkan true jika i adalah indeks yang valid utk ukuran list */
 /* yaitu antara indeks yang terdefinisi untuk container*/
@@ -95,7 +95,7 @@ boolean IsIdxValid (List L, IdxType i)
     return (FirstIdx(L) <= i && i <= LastIdx(L));
 }
 
-boolean IsIdxEff (List L, IdxType i)
+boolean IsIdxListEff (List L, IdxType i)
 /* Prekondisi : i sembarang*/
 /* Mengirimkan true jika i adalah indeks yang terdefinisi utk list */
 /* yaitu antara FirstIdx(L)..LastIdx(L) */
@@ -104,7 +104,7 @@ boolean IsIdxEff (List L, IdxType i)
 }
 
 /* ********** Operasi-operasi ********** */
-boolean Search(List L, ElType X)
+boolean SearchList(List L, ElType X)
 /* Prekondisi : X sembarang */
 /* Mengirimkan true jika terdapat elemen X di dalam list */
 /* yaitu antara FirstIdx(L)..LastIdx(L) */
@@ -124,7 +124,7 @@ boolean Search(List L, ElType X)
     return ketemu;
 }
 
-void InsertFirst(List *L, ElType X)
+void InsertFirstList(List *L, ElType X)
 /* I.S. L terdefinisi, mungkin kosong. */
 /* F.S. v menjadi elemen pertama L. */
 {
@@ -141,7 +141,7 @@ void InsertFirst(List *L, ElType X)
     }
 }
 
-void InsertAt(List *L, ElType X, IdxType i)
+void InsertListAt(List *L, ElType X, IdxType i)
 /* I.S. L terdefinisi, tidak kosong, i merupakan indeks lojik yang valid di L. */
 /* F.S. v disisipkan dalam L pada indeks ke-i (bukan menimpa elemen di i). */
 {
@@ -158,7 +158,7 @@ void InsertAt(List *L, ElType X, IdxType i)
     }
 }
 
-void InsertLast(List *L, ElType X)
+void InsertLastList(List *L, ElType X)
 /* I.S. L terdefinisi, mungkin kosong. */
 /* F.S. v menjadi elemen terakhir L. */
 {
@@ -170,7 +170,7 @@ void InsertLast(List *L, ElType X)
     } 
 }
 
-void DeleteFirst(List *L)
+void DeleteFirstList(List *L)
 /* I.S. L terdefinisi, tidak kosong. */
 /* F.S. F diset dengan elemen pertama L, elemen pertama L dihapus dari L. */
 {
@@ -187,7 +187,7 @@ void DeleteFirst(List *L)
     }
 }
 
-void DeleteAt(List *L, IdxType i)
+void DeleteListAt(List *L, IdxType i)
 /* I.S. L terdefinisi, tidak kosong, i merupakan indeks lojik yang valid di L. */
 /* F.S. Elemen L pada indeks ke-i dihapus dari L. */
 {
@@ -204,7 +204,7 @@ void DeleteAt(List *L, IdxType i)
     }
 }
 
-void DeleteLast(List *L)
+void DeleteLastList(List *L)
 /* I.S. L terdefinisi, tidak kosong. */
 /* F.S. F diset dengan elemen terakhir L, elemen terakhir L dihapus dari L. */
 {
@@ -214,7 +214,7 @@ void DeleteLast(List *L)
     }    
 }
 
-List Concat(List L1, List L2)
+List ConcatList(List L1, List L2)
 /* Prekondisi : L1 dan L2 tidak kosong */
 /* Mengirimkan sebuah List yang merupakan gabungan dari L1 dan L2 */
 /* Urutan elemen terisi dari L1, lalu L2 */
