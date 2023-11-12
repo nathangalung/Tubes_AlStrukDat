0d31a63c9a97a72f25cb33c161fa1a6ba36f63a6
nathangalung <bryanhutagalung246@gmail.com>
2023-11-12 12:59:34 +0700

    rev

--- program/FUNGSI/load.c
+++ program/FUNGSI/load.c
@@ -4,29 +4,32 @@
 
 void load(Word filename, Set *album, Map *album_artist, Map *song_album, DynamicList *user, Queue *queue, Stack *history, DynamicList *playlist)
 {
+    int count_artist = 0, count_album = 0, count_song = 0, count_user = 0, count_queue = 0, count_history = 0, count_playlist = 0;
+    Word name_artist, name_album, name_song, name_user, name_playlist;
+
     Word dir = {"FILE_CONFIG/", 12};
     filename = concat(dir, filename);
 
     StartWordNewline(filename);
-    int count_artist = word_to_int(currentWord);
+    count_artist = word_to_int(currentWord);
 
     for (int i=0; i<count_artist; i++)
     {
-        int count_album = read_word_count();
+        count_album = read_word_count();
         
-        Word name_artist = read_line_name();
+        name_artist = read_line_name();
         
         for (int j=0; j<count_album; j++)
         {
-            int count_song = read_word_count();
+            count_song = read_word_count();
 
-            Word name_album = read_line_name();
+            name_album = read_line_name();
             InsertMap(album_artist, name_album, name_artist);
             InsertSet(album, name_album);
 
             for (int k=0; k<count_song; k++)
             {                                           
-                Word name_song = read_line_name();
+                name_song = read_line_name();
                 InsertMap(song_album, name_song, name_album);
             }
         }
@@ -45,45 +48,45 @@ void load(Word filename, Set *album, Map *album_artist, Map *song_album, Dynamic
     {
     }
 
-
-    int count_user = read_line_count();
+    count_user = read_line_count();
 
     for(int i=0; i<count_user; i++)
     {
-        Word name_user = read_line_name();
+        name_user = read_line_name();
         InsertLastDynamic(user, name_user);
     }
 
     for(int i = 0; i < count_user; i++)
     {
-        int count_queue = read_line_count();
+        count_queue = read_line_count();
 
         for(int j=0; j<count_queue; j++)
         {
-            Word name_queue = read_line_name();
-            Enqueue(queue, name_queue);
+            name_song = read_line_name();
+            Enqueue(queue, name_song);
         }
 
-        int count_history = read_line_count();
+        count_history = read_line_count();
 
         for(int j=0; j<count_history; j++)
         {
-            Word name_history = read_line_name();
-            PushStack(history, name_history);
+            name_song = read_line_name();
+            PushStack(history, name_song);
         }
 
-        int count_playlist = read_line_count();
+        count_playlist = read_line_count();
 
         for(int j = 0; j < count_playlist; j++)
         {
-            int count_song = read_word_count();
-            Word name_playlist = read_line_name();
+            count_song = read_word_count();
+            name_playlist = read_line_name();
             InsertLastDynamic(playlist, name_playlist);
 
             for(int k = 0; k < count_song; k++)
             {
-                Word name_song = read_line_name();
+                name_song = read_line_name();
             }
         }
     }
+    printf("load lama berhasil\n");
 }
--- program/FUNGSI/start.c
+++ program/FUNGSI/start.c
@@ -4,37 +4,40 @@
 
 void start(Set *album, Map *album_artist, Map *song_album, DynamicList *user)
 {
+    int count_artist = 0, count_album = 0, count_song = 0, count_user = 0;
+    Word name_artist, name_album, name_song, name_user;
+
     Word filename = {"FILE_CONFIG/default.txt", 23};
     StartWordNewline(filename);
-    int count_artist = word_to_int(currentWord);
+    count_artist = word_to_int(currentWord);
 
     for (int i=0; i<count_artist; i++)
     {
-        int count_album = read_word_count();
+        count_album = read_word_count();
         
-        Word name_artist = read_line_name();
+        name_artist = read_line_name();
         
         for (int j=0; j<count_album; j++)
         {
-            int count_song = read_word_count();
+            count_song = read_word_count();
 
-            Word name_album = read_line_name();
+            name_album = read_line_name();
             InsertMap(album_artist, name_album, name_artist);
             InsertSet(album, name_album);
 
             for (int k=0; k<count_song; k++)
             {                                           
-                Word name_song = read_line_name();
+                name_song = read_line_name();
                 InsertMap(song_album, name_song, name_album);
             }
         }
     }
 
-    int count_user = read_line_count();
+    count_user = read_line_count();
 
     for(int i=0; i<count_user; i++)
     {
-        Word name_user = read_line_name();
+        name_user = read_line_name();
         InsertLastDynamic(user, name_user);
     }
 }
