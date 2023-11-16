dc730e17f273a2e0c05c67e7c5c0e34381e8a935
aninditaws <santosoanindita1@gmail.com>
2023-11-16 15:23:53 +0700

    update: play (belum fix)

--- program/FUNGSI/play.c
+++ program/FUNGSI/play.c
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 #include "play.h"
 
-int playSong(Map album_artist, Set album, Map song_album, Word song){
+int playSong(Map album_artist, Set album, Map song_album){
+    
     /* KAMUS LOKAL */
     int i, j;
     Word penyanyi, album_word, lagu;
@@ -18,13 +19,16 @@ int playSong(Map album_artist, Set album, Map song_album, Word song){
     printf("Masukkan Nama Penyanyi yang dipilih : ");
     StartWordMark();
 
+    // ADA STATIC LIST ARTIS
     penyanyi = currentWord;
-    printf("Daftar Album oleh %c : \n", penyanyi.TabWord);
+    printf("Daftar Album oleh ");
+    DisplayWord(penyanyi);
+    printf(": \n");
     for (i = 0; i < album_artist.Count; i++){
         if (CompareWord1(album_artist.Elements[i].Key, penyanyi)){
-            for (j = 0; j < album_artist.Elements[i].Value.Length; j++){
+            for (j = 0; j < album_artist.Count; j++){
                 printf("\t%d.", (j+1));
-                DisplayWord(album_artist.Elements[i].Value.TabWord[j]);
+                DisplayWord(album_artist.Elements[i].Value);
             }
         }
     }
@@ -32,12 +36,16 @@ int playSong(Map album_artist, Set album, Map song_album, Word song){
     StartWordMark();
 
     album_word = currentWord;
-    printf("Daftar Lagu Album %c oleh %c : \n", album_word.TabWord, penyanyi.TabWord);
+    printf("Daftar Lagu Album ");
+    DisplayWord(album_word);
+    printf(" oleh ");
+    DisplayWord(penyanyi);
+    printf(": \n");
     for (i = 0; i < song_album.Count; i++){
         if (CompareWord1(song_album.Elements[i].Key, album_word)) {
-            for (j = 0; j < song_album.Elements[i].Value.Length; j++) {
+            for (j = 0; j < song_album.Count; j++) {
                 printf("\t%d.", (j + 1));
-                DisplayWord(song_album.Elements[i].Value.TabWord[j]);
+                DisplayWord(song_album.Elements[i].Value);
             }
         }
     }
@@ -45,8 +53,14 @@ int playSong(Map album_artist, Set album, Map song_album, Word song){
     StartWordMark();
 
     lagu = currentWord;
-    printf("Memutar lagu %c oleh %c\n", lagu.TabWord, penyanyi.TabWord);
+    printf("Memutar lagu ");
+    DisplayWord(lagu);
+    printf(" oleh ");
+    DisplayWord(penyanyi);
+    printf("\n");
     return 0;
+
+    // clear queue
 }
 
 
--- program/FUNGSI/play.h
+++ program/FUNGSI/play.h
@@ -8,11 +8,12 @@
 #include "../ADT/list/array.h"
 #include "../ADT/machine/wordmachine.h"
 #include "../ADT/queuestack/stack.h"
+#include "../ADT/queuestack/queue.h"
 #include "../ADT/setmap/map.h"
 #include "../ADT/setmap/set.h"
 #include "../ADT/linkedlist/linier_list.h"
 
-int playSong(Map album_artist, Set album, Map song_album, Word song);
+int playSong(Map album_artist, Set album, Map song_album);
 
 int playPlaylist(LinierList playlist);
 
