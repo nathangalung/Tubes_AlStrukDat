c773d59ac64e3e0efd4fc5c9f996b91a48b14a78
aninditaws <santosoanindita1@gmail.com>
2023-11-21 11:12:28 +0700

    update: play, queue, help

--- program/FUNGSI/help.c
+++ program/FUNGSI/help.c
@@ -13,28 +13,29 @@ void help(boolean sesi, boolean menu)
     // ██║  ██║███████╗███████╗██║         ╚███╔███╔╝██║  ██║   ██║   ██║  ██║██║ ╚████║╚██████╔╝╚███╔███╔╝██║  ██║ ╚████╔╝ ███████╗\n\
     // ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝          ╚══╝╚══╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═╝  ╚═══╝  ╚══════╝\n\
     // ");
+    printf("\n==================================[ Menu Help WayangWave ]==================================");
     if (!sesi && !menu){ // perintah dipanggil sebelum memasuki sesi
-        printf("\t1. START -> Untuk masuk sesi baru \n");
-        printf("\t2. LOAD -> Untuk memulai sesi berdasarkan file konfigurasi \n");
-        printf("\t3. HELP -> Untuk menampilkan bantuan \n");
-        printf("\t4. QUIT -> Untuk keluar dari aplikasi \n");
+        printf("\n1. START -> Untuk masuk sesi baru \n");
+        printf("2. LOAD -> Untuk memulai sesi berdasarkan file konfigurasi \n");
+        printf("3. HELP -> Untuk menampilkan bantuan \n");
+        printf("4. QUIT -> Untuk keluar dari aplikasi \n");
     } else if(!sesi && menu){
-        printf("\t1. LOGIN -> Untuk masuk ke akun yang sudah ada \n");
-        printf("\t2. SIGN UP -> Untuk mendaftarkan akun \n");
-        printf("\t3. HELP -> Untuk menampilkan bantuan \n");
-        printf("\t4. SAVE -> Untuk menyimpan state aplikasi terbaru \n");
-        printf("\t5. QUIT -> Untuk keluar dari aplikasi \n");
+        printf("\n1. LOGIN -> Untuk masuk ke akun yang sudah ada \n");
+        printf("2. SIGN UP -> Untuk mendaftarkan akun \n");
+        printf("3. HELP -> Untuk menampilkan bantuan \n");
+        printf("4. SAVE -> Untuk menyimpan state aplikasi terbaru \n");
+        printf("5. QUIT -> Untuk keluar dari aplikasi \n");
     } else if (sesi && menu){ // perintah dipanggil setelah memasuki sesi
-        printf("\t1. LIST -> Untuk menampilkan daftar playlist, list penyanyi, list album, dan list lagu \n");    
-        printf("\t2. PLAY -> Untuk memutar lagu atau playlist yang dipilih \n");
-        printf("\t3. QUEUE -> Untuk memanipulasi queue lagu \n");
-        printf("\t4. SONG -> Untuk melakukan navigasi lagu yang ada pada queue \n");
-        printf("\t5. PLAYLIST -> Untuk melakukan basic command playlist seperti create, add, swap, remove, dan delete \n");
-        printf("\t6. STATUS -> Untuk menampilkan lagu yang sedang dimainkan, queue lagu yang ada, dan playlist lagu diputar \n");
-        printf("\t7. SAVE -> Untuk menyimpan state aplikasi terbaru \n");
-        printf("\t8. HELP -> Untuk menampilkan bantuan \n");
-        printf("\t9. QUIT -> Untuk keluar dari aplikasi \n");
-        printf("\t10. LOGOUT -> Untuk keluar dari sesi dan kembali ke menu utama \n");
+        printf("\n1. LIST -> Untuk menampilkan daftar playlist, list penyanyi, list album, dan list lagu \n");    
+        printf("2. PLAY -> Untuk memutar lagu atau playlist yang dipilih \n");
+        printf("3. QUEUE -> Untuk memanipulasi queue lagu \n");
+        printf("4. SONG -> Untuk melakukan navigasi lagu yang ada pada queue \n");
+        printf("5. PLAYLIST -> Untuk melakukan basic command playlist seperti create, add, swap, remove, dan delete \n");
+        printf("6. STATUS -> Untuk menampilkan lagu yang sedang dimainkan, queue lagu yang ada, dan playlist lagu diputar \n");
+        printf("7. SAVE -> Untuk menyimpan state aplikasi terbaru \n");
+        printf("8. HELP -> Untuk menampilkan bantuan \n");
+        printf("9. QUIT -> Untuk keluar dari aplikasi \n");
+        printf("10. LOGOUT -> Untuk keluar dari sesi dan kembali ke menu utama \n");
     }
 }
 
