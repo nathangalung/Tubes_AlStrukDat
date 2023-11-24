f6b8821e1cac5cf5cb48f6883ae64250f1e93c11
zhrsalsa <119563477+zhrsalsa@users.noreply.github.com>
2023-11-24 19:35:06 +0700

    keterangan

--- program/COMMAND/song.h
+++ program/COMMAND/song.h
@@ -8,7 +8,14 @@
 #include "../ADT/pcolor/pcolor.h"
 
 void SongNext(User *multi, StaticList artist, DynamicList *playing, int idx_user);
+/* 
+    I.S Memainkan sebuah lagu
+    F.S Lagu yang dimainkan berubah ke lagu selanjutnya yang terdapat pada queue. Aplikasi memutar ulang lagu yang sedang dimainkan jika tidak ada queue.
+*/
 
 void SongPrevious(User *multi, StaticList artist, DynamicList *playing, int idx_user);
-
+/*
+    I.S Memainkan sebuah lagu
+    F.S Lagu yang dimainkan berubah ke lagu sebelumnya yang ada di riwayat lagu. Aplikasi memutar ulang lagu yang sedang dimainkan jika tidak ada riwayat lagu.
+*/
 #endif
