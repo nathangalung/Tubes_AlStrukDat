4087dd9cf254390e85179aa77fd054bdbca9b788
aninditaws <santosoanindita1@gmail.com>
2023-11-24 15:47:02 +0700

    update penjelasan play & queue

--- program/COMMAND/play.h
+++ program/COMMAND/play.h
@@ -14,7 +14,14 @@
 #include "../ADT/pcolor/pcolor.h"
 
 void PlaySong(StaticList artist, Map album_artist, Map song_album, User *multi, DynamicList *playing, int idx_user);
+/* Memainkan lagu yang dipilih */
+/* Lagu terpilih menjadi Now Playing */
+/* Queue dan History kosong */
 
 void PlayPlaylist(User *multi, DynamicList *playing, int idx_user);
+/* Memainkan playlist yang dipilih */
+/* Now Playing berisi lagu pertama dari playlist yang dipilih */
+/* Queue lagu berisi lagu kedua hingga lagu terakhir dari playlist yang dipilih */
+/* History lagu berisi Queue namun dalam posisi yang terbalik */
 
 #endif
--- program/COMMAND/queue.h
+++ program/COMMAND/queue.h
@@ -9,13 +9,18 @@
 #include "../ADT/pcolor/pcolor.h"
 
 void QueueSong(StaticList artist, Map album_artist, Map song_album, User *multi, int idx_user);
+/* Memasukkan lagu yang dipilih ke dalam Queue */
 
 void QueuePlaylist(User *multi, int idx_user);
+/* Memasukkan semua lagu dari playlist yang dipilih ke dalam Queue */
 
 void QueueSwap(User *multi, Word word, int idx_user);
+/* Menukarkan urutan dua lagu pada queue */
 
 void QueueRemove(User *multi, Word word, int idx_user);
+/* Menhapus lagu dengan indeks yang dipilih dari queue */
 
 void QueueClear(User *multi, int idx_user);
+/* Mengosongkan queue */
 
 #endif
